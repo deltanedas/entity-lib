c642823e16dcfdf2bfbd390ea3c20d750fbf793f
DeltaNedas <deltanedas@gmail.com>
2020-01-30 17:21:56 +0000

    mmmmmmmmmmmmmmmmmmmmmmmmmm

--- scripts/init.js
+++ scripts/init.js
@@ -3,3 +3,6 @@ this.global.entityLib = {};
 print("require: " + require)
 print(require("common.js"))
 print(require("common.js")())
+require("common.js")();
+require("mech.js")();
+require("unit.js")();
