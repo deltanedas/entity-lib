0ac7a6715bfe01a8fb0b093ad31519dd9c99cfc7
DeltaNedas <deltanedas@gmail.com>
2020-01-31 07:25:37 +0000

    e

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,9 @@
 print("init")
 this.global.entityLib = {};
+
+g = new Packages.org.mozilla.javascript.tools.shell.Global(Packages.org.mozilla.javascript.Context.getCurrentContext());
+const require = g.load;
+
 print("require: " + require)
 print(require("common.js"))
 print(require("common.js")())
