ab217d11e69f34fcd064f9981fba62cb55db7c33
DeltaNedas <deltanedas@gmail.com>
2020-03-08 00:19:26 +0000

    egg

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 if (this.global.entityLib === undefined) {
 	this.global.entityLib = {features: {}};
 
-	require("common");
+	require("entity-lib/common");
 	require("mech");
 	require("unit");
 	require("wrapper");
